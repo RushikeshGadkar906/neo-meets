39bfd0f03894ff82d2934a5ad8de052face4ce33
Rutuja Dond <rutuja.dond@neosoftmail.com>
2025-06-25 11:09:39 +0530

    create mui setup

--- frontend/src/OwnComponents/BasicButtons.tsx
+++ frontend/src/OwnComponents/BasicButtons.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import { Typography, useTheme } from '@mui/material';
 
 export default function BasicButtons() {
+  const theme = useTheme()
+
   return (
     <Stack spacing={2} direction="row">
-      <Typography variant='caption'>Rutuja</Typography>
+      <Typography variant='caption' sx={{color: theme.palette.grey[100]}}>Rutuja</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
